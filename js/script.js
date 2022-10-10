8f1f1fd60cc4c65469c264cbf037d916c2cb8431
Mikelangelo64 <donni2001331@gmail.com>
2022-10-10 17:05:14 +0300

    v-0.1

---
+++ js/script.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+
+    const isMobile = {
+        Android: function(){
+            return navigator.userAgent.match(/Android/i)
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i)
+        },
+        iOS: function(){
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i)
+        },
+        Opera: function(){
+            return navigator.userAgent.match(/Opera mini/i)
+        },
+        Windows: function(){
+            return navigator.userAgent.match(/IEMobile/i)
+        },
+        any: function(){
+            return(
+                isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows()
+            )
+        }
+    }
+
+    if(isMobile.any()){
+        $('body').addClass('_touch')
+
+    }else{
+        $('body').addClass('_pc')
+    }
+
+    //scroll
+    let sections = Array.from(document.querySelectorAll('.section'))
+    let index = 0
+    let currentSection = sections[index]
+    function slide() {
+        h = document.documentElement.clientHeight
+        $('.section').css('height', h)
+    }
+
+    $(window).resize(slide)
+    $(document).ready(slide)
+    let currentScroll = window.scrollY
+    let lastScroll = currentScroll
+    let ticking = true
+
+    function listener(evt){
+        lastScroll = currentScroll
+        currentScroll = window.scrollY 
+        console.log(currentScroll, lastScroll, ticking);
+
+        if(ticking) {
+            if(lastScroll - currentScroll < 0) {
+                console.log('down');
+                index = Math.min(index + 1, sections.length - 1)
+            }
+            if(lastScroll - currentScroll > 0) {
+                console.log('up');
+                index = Math.max(index - 1, 0)
+            }
+            console.log(index);
+
+            if (index % 2 === 0) {
+                makeWhite('.header')
+                makeWhite('.button-svg')
+            } else {
+                makeBlack('.header')
+                makeBlack('.button-svg')
+            }
+            //console.log(currentScroll);
+            // setTimeout(() => {
+            //     //ticking = true
+            //     // document.addEventListener('scroll', listener)
+            // }, 500);
+        }
+        
+        document.removeEventListener('scroll', listener)
+        $('html, body').animate({
+            scrollTop: $(sections[index]).offset().top
+        }, 300, "linear", () => {
+            currentScroll = window.scrollY
+            ticking = true
+            document.addEventListener('scroll', listener)
+        })
+    }
+    if(!isMobile.any()){
+        document.addEventListener('scroll', listener)
+    }
+
+    //menu open
+    $('.menu-burger').click(function(e){
+        e.preventDefault()
+        $(this).toggleClass('_active')
+        $('.menu').toggleClass('_active')
+    })
+
+    //make white/black header/footer
+    function makeWhite(section) {
+        $(section).removeClass('_black')
+        $(section).addClass('_white')
+    }
+    function makeBlack(section) {
+        $(section).removeClass('_white')
+        $(section).addClass('_black')
+    }
+
+    makeWhite('.header')
+    makeWhite('.button-svg')
+    if(isMobile.any()) {
+        sections.forEach((section, index) => {
+            callback = (entries, observer) => {
+                if (entries[0].isIntersecting) {
+                    //console.log(index, entries);
+                    if (index % 2 === 0) {
+                        makeWhite('.header')
+                        makeWhite('.button-svg')
+                    } else {
+                        makeBlack('.header')
+                        makeBlack('.button-svg')
+                    }
+                }
+            }
+            let observerHead = new IntersectionObserver(callback, {threshold: 0.5})
+            //let observerFoot = new IntersectionObserver(callback, {threshold: 0.2})
+            observerHead.observe(section)
+            //observerFoot.observe(section)
+        })
+    }
+})
